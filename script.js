f4e8506ec479756d1d912aa146873b4b23a8fb3e
Lucas <lucashenriquecardosoprado@gmail.com>
2026-09-11 17:15:58 -0300

    creating checkbox and using checked to change style

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let taskCount = 1;
 form.addEventListener('submit', function(event) {
     // prevent the default action of the form submitting (refreshing the page)
     event.preventDefault();
+
+    // CREATING OBJECT
+
     // creating new object to store the task data
     let obj = new Object();
     // creating a property for the task status
@@ -20,15 +23,31 @@ form.addEventListener('submit', function(event) {
     taskCount++;
     // adding the object to the tasks array
     tasks.push(obj);
+
+    // CREATING LIST
+
     // creating new list item
     const newItem = document.createElement('li');
+    newItem.className = 'task'
+    // creating new checkbox
+    const checkItem = document.createElement('input');
+    // setting the type of the input to checkbox
+    checkItem.type = 'checkbox';
+    // creating event listener for the checkbox
+    checkItem.addEventListener('change', function() {
+        obj.finished = checkItem.checked;
+    });
     // creating new text to connect with the list item
     const newContent = document.createTextNode(obj.task);
+    // connecting list item with the checkbox
+    newItem.appendChild(checkItem);
     // connecting list item with the text
     newItem.appendChild(newContent);
     // connecting list item with the ul list
     list.appendChild(newItem);
-    
+
+    // DEBUGGING
+
     //getting the last item of the tasks array and printing it for testing.
     console.log(tasks[tasks.length - 1]);
 })
